abaa09cedcc218c733cf2935add665fe76e732d4
gaeun1747 <gaeun931@gmail.com>
2016-10-25 19:38:25 +0900

    test

--- planet/src/com/my/dao/ArticleDAO.java
+++ planet/src/com/my/dao/ArticleDAO.java
@@ -8,20 +8,20 @@ public interface ArticleDAO {
 	//게시글 추가
 	public void insertArticle(Article a);
 	//댓글 추가
-	public void insertComment(Article a);
+	public void insertComment(Article a, int article_id);
 	//게시글, 댓글 삭제
 	public void delete(int art_id);
 	//게시글, 댓글 내용 수정
-	public void update(int art_id);
+	public void update(Article a);
 	//중요도 수정
-	public void updateImp(int art_id);
+	public void updateImp(int art_id, int imp);
 	//공지사항 수정
-	public void updateNotice(int art_id);
+	public void updateNotice(int art_id, char notice);
 	/**
 	 * @param art_id
 	 * @return 삭제를 위해 특정 게시글 조회
 	 */
-	public Article searchById(int art_id); 
+	public Article selectById(int art_id); 
 	/**
 	 * @param planet_id
 	 * @return 플래닛 별로 모든 그룹의 게시글을 조회
@@ -32,6 +32,9 @@ public interface ArticleDAO {
 	 * @return 특정 그룹내 모든 게시글 조회
 	 */
 	public List<Article> selectByNation(int nation_id);
-	//게시글 id 별 조회
-	public List<Article> selectById(int art_id);
+	/**
+	 * @param member_id
+	 * @return 내가 쓴 글 조회
+	 */
+	public List<Article> selectByMemId(int member_id);
 }
--- planet/src/com/my/dao/Plan_ArtDAO.java
+++ planet/src/com/my/dao/Plan_ArtDAO.java
@@ -13,8 +13,8 @@ public interface Plan_ArtDAO {
 	public void update(Plan_Art p);
 	//그룹별 조회
 	public List<Plan_Art> selectByNation(int nation_id);
-	//게시글 id로 조회 - 게시글 삭제시 연쇄적으로 상태변경위해
-	public List<Plan_Art> selectById(int art_id);// 일정이 하나밖에 없을 경우 리턴타입?
+	//게시글 id로 조회
+	public Plan_Art selectById(int art_id);// 일정이 하나밖에 없을 경우 리턴타입?
 	
 	
 }
